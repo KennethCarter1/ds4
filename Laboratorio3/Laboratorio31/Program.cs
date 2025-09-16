52028f76f705093863c5fd9d3075ddd804537456
Kenneth Carter <kenneth.jared1004@gmail.com>
2025-09-16 16:05:27 -0500

    Entrega del Laboratorio3

---
+++ Laboratorio3/Laboratorio31/Program.cs
@@ -0,0 +1,24 @@
+﻿using Laboratorio31;
+using System;
+internal class Program
+
+{
+    private static void Main(string[] args)
+    {
+        double Numero1, Numero2, Respuesta;
+
+        CalculosMatematicos calc = new CalculosMatematicos();
+
+        Console.Write("Introduce el primer numero:");
+        Numero1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduce el segundo numero:");
+        Numero2 = Convert.ToInt32(Console.ReadLine());
+
+        Respuesta = calc.Calcular(Numero1, Numero2);
+
+        Console.WriteLine("La operacion de ({0}+{1})({0}-{1}) es : {2}", Numero1, Numero2, Respuesta);
+    }
+
+
+}
---
+++ Laboratorio3/Laboratorio33/Program.cs
@@ -0,0 +1,23 @@
+﻿using Laboratorio31;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double Base, altura, resultado;
+        CalculosMatematicos calc = new CalculosMatematicos();
+
+        Console.Write("Introduce el primer numero:");
+        Base = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Introduce el segundo numero:");
+        altura = Convert.ToInt32(Console.ReadLine());
+
+        resultado = calc.perimetroRectangulo(Base, altura);
+
+        Console.WriteLine("El perimetro es: {0}", resultado);
+
+
+    }
+}
