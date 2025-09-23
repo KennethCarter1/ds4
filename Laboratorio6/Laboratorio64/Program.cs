05f36687580ab44b404f9c399bb18e13f172cf6e
Kenneth Carter <kenneth.jared1004@gmail.com>
2025-09-22 19:54:13 -0500

    Entrega del Laboratorio 6

---
+++ Laboratorio6/Laboratorio64/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void checkAge(int age)
+    {
+        if (age < 18)
+        {
+            throw new ArithmeticException("Accesso negado - No cumple con el criterio de edad");
+        }
+        else
+        {
+            Console.WriteLine("Accesso Concedido");
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        checkAge(15);
+    }
+}
