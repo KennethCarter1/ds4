fa26efd76184ca0943bbd51131eff59873b5cb85
Kenneth Jared Carter Mina <kenneth.jared1004@gmail.com>
2025-11-06 17:38:36 -0500

    Entrega del Laboratorio19

---
+++ Laboratorio19/Laboratorio19/Controllers/AccessController.cs
@@ -0,0 +1,18 @@
+﻿using Laboratorio19.Models.WS;
+using System.Web.Http;
+
+namespace Laboratorio191.Controllers
+{
+    public class AccessController : ApiController
+    {
+        [HttpGet]
+        public Reply HelloWorld()
+        {
+            Reply oR = new Reply();
+            oR.result = 1;
+            oR.data = null;
+            oR.message = "Mi Hello World en API";
+            return oR;
+        }
+    }
+}
